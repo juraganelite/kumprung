4e59632e42cc011125883a11dbe4c516a53d8830
juraganelite <101795987+juraganelite@users.noreply.github.com>
2022-12-02 21:12:16 +0700

    Create 728x90.js

---
+++ 728x90.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '8bb56f35fd23130c4d85930aab1c6903',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://bringsconserve.com/8bb56f35fd23130c4d85930aab1c6903/invoke.js"></scr' + 'ipt>');
